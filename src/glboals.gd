0d05ad3ffcf5d7ec30d8d1dbd337ad4dd2f61274
mequam <blue9ja@gmail.com>
2026-07-25 17:32:15 -0400

    main menu basics done

--- src/glboals.gd
+++ src/glboals.gd
@@ -1,5 +1,5 @@
 extends Node
 #class_name Glboals
-@export var roc_colur : Color = Color.DEEP_PINK
+@export var roc_colur : Color = Color.from_hsv(randf(),1.0,1.0)
 @export var hit_times : PackedFloat64Array
 @export var level_seed : int = 3
