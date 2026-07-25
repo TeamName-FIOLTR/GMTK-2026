7b8f151b8d60d21f6fec4f270a4edc76812edcbc
XGit3000 <sebastian.carrier@gmail.com>
2026-07-25 16:21:11 -0400

    gampley2

--- src/glboals.gd
+++ src/glboals.gd
@@ -1,5 +1,8 @@
 extends Node
 #class_name Glboals
-@export var roc_colur : Color = Color.DEEP_PINK
-@export var hit_times : PackedFloat64Array
+@export var roc_colur : Color = Color.LAWN_GREEN
+@export var hit_positions : PackedFloat64Array = [1]
 @export var level_seed : int = 3
+
+@export var LEVEL_FAILED_SCREEN : PackedScene = preload("res://scene/level_failed_screen.tscn")
+@export var LEVEL_PASSED_SCREEN : PackedScene = preload("res://scene/level_passed_screen.tscn")
