caeb66abf1ebd855ca70691f54d3e167ea156c7c
mequam <blue9ja@gmail.com>
2026-07-25 16:09:02 -0400

    fall animation :D

--- src/scenes/player_animation_tree.gd
+++ src/scenes/player_animation_tree.gd
@@ -3,7 +3,8 @@ extends AnimatedSprite2D
 
 func _ready() -> void:
 	self.animation_finished.connect(self.on_anim_finished)
+	self.play("fall")
 
-func on_anim_finished(anim)->void:
-	if anim != 'fall':
+func on_anim_finished()->void:
+	if self.animation != 'fall':
 		self.play('fall')
