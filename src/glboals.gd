5ab8db826788fe6176eed76b447de1a03b0d3561
mequam <blue9ja@gmail.com>
2026-07-25 22:34:32 -0400

    INFINIT levels

--- src/glboals.gd
+++ src/glboals.gd
@@ -1,8 +1,19 @@
 extends Node
 #class_name Glboals
 @export var roc_colur : Color = Color.from_hsv(randf(),1.0,1.0)
-@export var hit_positions : PackedFloat64Array = [1]
 @export var level_seed : int = 3
+
+@export var hit_positions : PackedFloat64Array = [1]
 
 @export var LEVEL_FAILED_SCREEN : PackedScene = preload("res://scene/level_failed_screen.tscn")
 @export var LEVEL_PASSED_SCREEN : PackedScene = preload("res://scene/level_passed_screen.tscn")
+
+
+func generate_new_level():
+	Glboals.level_seed += 1
+	seed(Glboals.level_seed)
+
+	Glboals.roc_colur = Color.from_hsv(randf(),1,1)
+	self.hit_positions = [1]
+	for i in range(int(randf()*5) + Glboals.level_seed*3):
+		self.hit_positions.append(self.hit_positions[-1] + randf()*0.3 + 0.1)
--- src/scenes/world.gd
+++ src/scenes/world.gd
@@ -43,11 +43,12 @@ func spawn_all_thi_hits():
 		var roc : Roc = preload("res://scenes/roc.tscn").instantiate()
 		$rocs.add_child(roc)
 		roc.position.x = rng.randf_range(0.1,0.9)*get_viewport_rect().size.x
-		roc.position.y = -$rocs.position.y+get_viewport_rect().size.y#*h*60/323
+		roc.position.y = -$rocs.position.y+get_viewport_rect().size.y*h#*60/323
 		end_y = max(roc.position.y,end_y)
 		pass
 	pass
 	roc_count = len(Glboals.hit_positions)
+	$CanvasLayer/Control/Label.text = "%s rocs left" % roc_count
 	level_limits = end_y+4*get_viewport_rect().size.y
 
 @export var hit_sound : AudioStream
@@ -59,7 +60,7 @@ func test_click() -> bool:
 	var hit : Vector2 = player.get_collision_point()
 	if hit.y - player_y > 300: return false
 	player_y = hit.y
-	var proportion : float = pow(player_y/get_viewport_rect().size.y,1/3.0)
+	#var proportion : float = pow(player_y/get_viewport_rect().size.y,1/3.0)
 	
 	v = -jump_v #*proportion
 	var sfx = SoundEffecticle.get_sound_effect_node(hit_sound)
@@ -67,12 +68,15 @@ func test_click() -> bool:
 	sfx.global_position = roc.global_position
 	sfx.pitch_scale = pow(2.0,randf_range(-0.05,0.05)-1)
 	sfx.play()
+
+	roc_count = $rocs.get_child_count() - 1 #queue free takes a moment to take effect
 	roc.queue_free()
 	var rocticles : Rocticles = rocticles_scene.instantiate()
 	add_child(rocticles)
 	rocticles.emitting = true
 	rocticles.global_position = roc.global_position
-	roc_count = $rocs.get_child_count()
+
+	print_debug(roc_count)
 	shake_amp = 10
 	$CanvasLayer/Control/Label.text = "%s rocs left" % roc_count
 	
@@ -81,6 +85,7 @@ func test_click() -> bool:
 	#var global_pos = player.global_position
 	#var tolarence : float = 50
 	#var hi_david : float = 100
+	#var hi_max : float = 102
 	#for roc : Roc in $rocs.get_children():
 		#var roc_pos = roc.global_position
 		#if abs(roc_pos.x-global_pos.x) > tolarence: continue
@@ -96,7 +101,10 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if (event.button_index == MOUSE_BUTTON_LEFT or event.button_index == MOUSE_BUTTON_RIGHT) and event.is_pressed():
 			print(test_click())
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("restart"):
+		get_tree().reload_current_scene()
+	if passed and event.is_action_pressed("continue"):
+		Glboals.generate_new_level()
 		get_tree().reload_current_scene()
 	
 
@@ -106,12 +114,15 @@ var t = 999990.0
 var v = 0
 
 var judged : bool = false
+var passed : bool = false
 
 func judge_my_vow_shynx_of_black_quartz():
+	#generate_new_level()
 	if $rocs.get_child_count() > 0:
 		$CanvasLayer.add_child(Glboals.LEVEL_FAILED_SCREEN.instantiate())
 	else:
 		$CanvasLayer.add_child(Glboals.LEVEL_PASSED_SCREEN.instantiate())
+		passed = true
 	judged = true
 
 func _physics_process(delta: float) -> void:
@@ -134,9 +145,10 @@ func _physics_process(delta: float) -> void:
 		t = 0
 		next_t = rng.randf_range(0,1.0)
 	pass
-	if $Camera2D.position.y == level_limits and not judged:
+	
+	#error term for aproximations resulting from lerping
+	if $Camera2D.position.y >= level_limits - 0.01 and not judged:
 		judge_my_vow_shynx_of_black_quartz()
-
 
 
 #var hit_things_test : PackedFloat64Array = [0.0, 4.0, 8.0, 12.0, 14.0, 16.0, 20.0, 21.0, 22.0, 22.5, 23.0, 24.0, 28.000001907348633, 29.000001907348633, 30.000001907348633, 30.500001907348633, 32.0, 34.0, 36.0, 36.5, 38.0, 40.0, 41.0, 42.0, 43.0, 44.0, 44.5, 45.0, 45.5, 46.0, 47.0, 48.0, 48.5, 49.0, 50.0, 50.5, 51.0, 52.0, 52.5, 53.0, 53.5, 54.0, 54.5, 55.0, 55.5, 56.0, 57.0, 57.5, 58.0, 60.0, 60.5, 61.0, 62.0]
